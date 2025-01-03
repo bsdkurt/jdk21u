797f7447abd614a84e7f035b1cee5101c23438e8
Harald Eilertsen <haraldei@anduin.net>
2025-01-03 14:13:21 +0100

    Variuous undefined local var fixed for BSD.

--- src/jdk.hotspot.agent/bsd/native/libsaproc/ps_core.c
+++ src/jdk.hotspot.agent/bsd/native/libsaproc/ps_core.c
@@ -991,7 +991,8 @@ static bool read_shared_lib_info(struct ps_prochandle* ph) {
   }
 
   // read ld_base address from struct r_debug
-#if 0  // There is no r_ldbase member on BSD
+#if defined(__FreeBSD__)
+  uintptr_t ld_base_addr;
   if (ps_pread(ph, (psaddr_t) debug_base + LD_BASE_OFFSET, &ld_base_addr,
                   sizeof(uintptr_t)) != PS_OK) {
     print_debug("can't read ld base address\n");
