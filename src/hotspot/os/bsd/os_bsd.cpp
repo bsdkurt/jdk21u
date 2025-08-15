ff2be9d4ae897604de43e1a1693bfd7000d10d41
Kurt Miller <bsdkurt@gmail.com>
2025-08-15 11:53:39 -0400

    Use os::Bsd::gettid() in all cases.

--- src/hotspot/os/bsd/os_bsd.cpp
+++ src/hotspot/os/bsd/os_bsd.cpp
@@ -906,13 +906,7 @@ pid_t os::Bsd::gettid() {
 }
 
 intx os::current_thread_id() {
-#ifdef __APPLE__
   return (intx)os::Bsd::gettid();
-#elif defined(__FreeBSD__)
-  return (intx)os::Bsd::gettid();
-#else
-  return (intx)::pthread_self();
-#endif
 }
 
 int os::current_process_id() {
